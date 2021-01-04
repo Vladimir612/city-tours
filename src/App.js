ee2426ef1e4dd8c9d767ded0779dd636c36b422e
unknown <vladimir.12.lazarevic@gmail.com>
2021-01-04 20:50:32 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.sass'
+import { Navbar } from './components'
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
